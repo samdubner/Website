4ff744cf3923fb8950ba5d107625155e12defa08
SamuelDub <dubnersamuel@yahoo.com>
2016-10-07 16:00:37 -0700

    starts actual chat, implements better fixes

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 function sendMessage(){
-	var textBubble = $(document.createElement("p"));
+	var textbubble = $(document.createElement("p"));
 	var div = $(document.createElement("div"));
 	div.addClass("right-align");
+	var textbubblel = $(document.createElement("p"));
+	var divl = $(document.createElement("div"));
+	divl.addClass("left-align");
 	var text = $("#textbox-text").val();
-	if(text.length > 190){
-		alert("Your message is too long.");
+	if(text == ""){
+		
 	} else {
-		if(text == "") {
-			alert("Please do not send an empty message");
-		} else {
-			textBubble.html(text);
-			textBubble.addClass("bubble");
-			div.append(textBubble)
-			$(".text-display").append(div);
-			$(".text-display").scrollTop($(".text-display")[0].scrollHeight);
-			$("#textbox-text").val("");
-		}
-
+		textbubblel.html(text);
+		textbubble.html(text);
+		textbubblel.addClass("bubble-left")
+		textbubble.addClass("bubble-right");
+		divl.append(textbubblel);
+		div.append(textbubble);
+		$(".text-display").append(divl);
+		$(".text-display").append(div);
+		$(".text-display").scrollTop($(".text-display")[0].scrollHeight);
+		$("#textbox-text").val("");
 	}
-
 }
 
 function setName(){
@@ -29,11 +30,23 @@ function setName(){
 	$(".username-div").append(setName);
 }
 
-$('#textbox-text').on('keydown', function(e) {
+$("#textbox-text").on("keydown", function(e) {
     if (e.which == 13) {
         e.preventDefault();
 		$("#submit-button").trigger("click");
     }
+});
+
+$("#textbox-text").change(function(){
+	
+});
+$(document).on("change keyup paste mouseup", "#textbox-text", function(){
+	var text = $("#textbox-text").val();
+	if(text == ""){
+		$("#submit-button").attr("disabled", true);
+	} else{
+		$("#submit-button").attr("disabled", false);
+	}
 });
 
 $("#submit-button").on("click", function(){
